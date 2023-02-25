2c20a3078ea697572fe0b1d1ee7044cf911c5da6
lbluefishl <lbluefishl@yahoo.com>
2023-02-26 10:45:38 +1100

    test

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-const video = document.getElementById("video");
+const videos = document.querySelectorAll('[id^="v"]');
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    // If the video is in the viewport
-    if (entries[0].isIntersecting) {
-      video.play(); // Start playing the video
-    } else {
-      video.pause(); // Pause the video
-    }
-  },
-  {
-    threshold: 0.5, // The video will start playing when it is 50% in the viewport
+// Create an IntersectionObserver for each video element
+videos.forEach(video => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // If the video is intersecting the viewport
+      if (entry.isIntersecting) {
+        video.play(); // Play the video
+      } else {
+        // If the video is not intersecting the viewport and is not paused
+        if (!video.paused && entry.intersectionRatio === 0) {
+          video.load(); // Reload the video
+          video.pause(); // Pause the video
+        } else {
+          video.pause(); // Pause the video
+        }
+      }
+    });
+  }, { threshold: 0 });
+
+  observer.observe(video);
+
+  // Get the video container element for this video
+  const videoContainer = document.querySelector(`.video-container[data-video="${video.id}"]`);
+  if (videoContainer) {
+    // Set the height of the video container to match the video height
+    videoContainer.style.height = video.videoHeight + 'px';
   }
-);
-
-observer.observe(video);
+});
