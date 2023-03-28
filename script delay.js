4c9ee3ddbd1dc8a49ece42229d33e0796124a08f
lbluefishl <lbluefishl@yahoo.com>
2023-03-28 12:35:23 +1100

    add more videos

--- script delay.js
+++ script delay.js
@@ -1,26 +1,30 @@
- const videos = document.querySelectorAll('[id^="v"]');
+const videos = document.querySelectorAll('[id^="v"]');
 
 // Create an IntersectionObserver for each video element
 videos.forEach(video => {
+  let timeoutId; // Initialize timeout ID variable
+
   const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
       // If the video is intersecting the viewport
       if (entry.isIntersecting) {
-
-        setTimeout(function() {
-          video.play();
-        }, 2000);
-        // Play the video
-        
+        // If the timeout has not been cleared, play the video after the delay
+        if (!timeoutId) {
+          timeoutId = setTimeout(() => {
+            video.play();
+          }, 2000);
+        }
       } else {
         // If the video is not intersecting the viewport and is not paused
-        
         if (!video.paused && entry.intersectionRatio < 1) {
           video.load(); // Reload the video
           video.pause(); // Pause the video
-      
+          clearTimeout(timeoutId); // Clear the timeout
+          timeoutId = null; // Reset the timeout ID
         } else {
           video.pause(); // Pause the video
+          clearTimeout(timeoutId); // Clear the timeout
+          timeoutId = null; // Reset the timeout ID
         }
       }
     });
@@ -36,7 +40,6 @@ videos.forEach(video => {
   }
 });
 
-
-setTimeout(function(){
+setTimeout(() => {
   window.location = "survey.html";
 }, 180000);
